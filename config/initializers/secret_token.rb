f5b7e408f6f1976115743cca848f45d5d736d607
Yury Azarenok <gorilla33@mail.ru>
2012-04-08 22:14:18 +0300

    Add model Pictures

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lazonga::Application.config.secret_token = '222a3993c3c286604f878a570993f39445894461b6262e6e3422db56db8e170378322f9a0e1f907bb5e650fc89916661f8fb4bbc6ed248df7ebe4fe8a2e415bd'
